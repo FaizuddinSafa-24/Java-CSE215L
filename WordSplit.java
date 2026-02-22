eec7bf0af9f043f4022d1842dc39ee22823b7760
FaizuddinSafa-24 <safarajuk19@gmail.com>
2026-02-22 08:00:04 +0000

    Split with array

--- WordSplit.java
+++ WordSplit.java
@@ -1,4 +1,19 @@
+import java.util.Scanner;
+
 public class WordSplit {
 
-    
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        while (true) {
+            String input = scan.nextLine();
+            if (input.isEmpty()) {
+                break;
+            }
+            String[] arr = input.split(" ,");
+            for (String word : arr) {
+
+                System.out.println(word);
+            }
+        }
+    }
 }
